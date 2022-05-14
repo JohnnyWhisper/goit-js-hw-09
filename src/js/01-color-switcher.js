1effa5ee2e03fa650dca29380cfa338f40544005
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-15 02:17:15 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const INTERVAL_TIME = 1000;
+let intervalId = null;
+
+buttonStop.disabled = true;
+
+const startChangingColor =()=> {
+    intervalId = setInterval( () => {
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+        buttonStart.disabled = true;
+        buttonStop.disabled = false;
+    }, INTERVAL_TIME);
+};
+
+
+const stopChangingColor = () => {
+   clearInterval(intervalId);
+   buttonStart.disabled = false;
+   buttonStop.disabled = true;
+}
+
+
+buttonStart.addEventListener('click', startChangingColor);
+buttonStop.addEventListener('click', stopChangingColor);
