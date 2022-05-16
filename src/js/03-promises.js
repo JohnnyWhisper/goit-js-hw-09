f9d804c4735c8fb48f6bc3fad2563b5d23402fdb
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-17 00:44:50 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+
+formEl.addEventListener('submit', onCreatePromises);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromises(event) {
+  event.preventDefault();
+
+  let step = Number(event.currentTarget.step.value);
+  let delay = Number(event.currentTarget.delay.value);
+  let amount = Number(event.currentTarget.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(success => console.log('✅ Fulfilled promise'))
+      .catch(error => console.log('❌ Rejected promise'));
+    delay += step;
   }
 }
+
